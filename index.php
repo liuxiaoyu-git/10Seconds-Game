e408eb51a0b6d93d8b6ce1cf5202dd12e70dab32
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-11-04 00:09:58 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <table width="100%" height="100%">
+            <tr>
+                <td align="center">
+                    <a href="10seconds.php">1）去玩</a><br/>
+                    <a href="showwinner.php">2）显示结果列表</a><br/>
+                    <a href="initapp.php">3）重置数据库（清除所有数据）</a>
+                </td>
+	        </tr>
+        </table>
+    </body>
+</html>
